4a16a81c1240064c3de58e84a9ccc206823b235f
jc <jym@jym.sg>
2018-05-08 10:08:56 +0800

    replace negative number
    negative number cause insert to fail

--- 5. Threat Analytics/orientDB/injectsysmon.py
+++ 5. Threat Analytics/orientDB/injectsysmon.py
@@ -10,7 +10,8 @@ filepath = 'template eventlog.txt'  # change to your file path
 lines = codecs.open(filepath, 'r', encoding='utf-8').readlines()
 for event in lines:
     e = json.loads(event)
-    client.command("insert into Sysmon content " + event)
+    e['Keywords'] = ''
+    client.command("insert into Sysmon content " + json.dumps(e))
     if("ParentProcessGuid" in event):
         edgecmd = "Create Edge ParentOf FROM (Select from Sysmon Where EventID = 1 AND ProcessGuid = '{0}' AND Hostname = '{1}') TO (SELECT FROM Sysmon Where ProcessGuid = '{2}' AND Hostname = '{3}')".format(e["ParentProcessGuid"],e['Hostname'],e["ProcessGuid"],e['Hostname'])
         try:
