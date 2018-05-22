3b3a51942ef723704f19defdf2bb6fe3b95eb6ed
jymcheong <jym@jym.sg>
2018-05-22 16:34:07 +0800

    lots of work..

--- 5. Threat Analytics/orientDB/createEdges.js
+++ 5. Threat Analytics/orientDB/createEdges.js
@@ -2,7 +2,7 @@ var OrientDB = require('orientjs');
 var server = OrientDB({host: 'localhost', port: 2424});
 var db = server.use({name: 'DataFusion', username: 'root', password: 'Password1234', useToken : true});
 
-//build ProcessCreate Parent-Child relationships
+//create ProcessCreate Parent-Child relationships
 db.liveQuery("live select from ProcessCreate")
   .on('live-insert', function(data){
      var child = data.content;
@@ -23,3 +23,34 @@ db.liveQuery("live select from ProcessCreate")
                     console.log('no parent found...')
             });
   })
+
+// Legend: FromClass-[EdgeClassName:PropertiesToLinkWith]->ToClass
+
+// Stage 2 - Run payload eg. MSF Process Migration creates remote threads
+// ProcessCreate-[CreatedRemoteThread:SourceProcessGuid]->CreateRemoteThread
+// CreateRemoteThread-[RemoteThreadFor:TargetProcessId]->ProcessCreate
+
+// Stage2 - Install Payload / Persistence
+// ProcessCreate-[WroteFile:ProcessGuid,Hostname]->FileCreate
+// FileCreate-[UsedAsDriver:TargetFilename=ImageLoaded]->DriverLoad
+// FileCreate-[UsedAsImage:TargetFilename=ImageLoaded]->ImageLoad
+// ProcessCreate-[LoadedImage:ProcessGuid,Hostname]->ImageLoad
+// ProcessCreate-[AccessedRegistry:ProcessGuid,Hostname]->RegistryEvent
+// ProcessCreate-[CreatedFileStream:ProcessGuid,Hostname]->FileCreateStreamHash
+// ProcessCreate-[AccessedWMI:ProcessGuid,Hostname]->WmiEvent
+// ProcessCreate-[Terminated:ProcessGuid,Hostname]->ProcessTerminate
+
+// Stage2/3 External-Internal C2
+// ProcessCreate-[ConnectedTo:ProcessGuid,Hostname]->NetworkConnect
+// ProcessCreate-[CreatedPipe:ProcessGuid,Hostname]-> PipeCreate
+// ProcessCreate-[ConnectedPipe:ProcessGuid,Hostname]->PipeConnected
+
+// Stage3 Capture Credentials - eg. Mimikatz
+// ProcessCreate-[ProcessAccessed:SourceProcessGuid]->ProcessAccess
+// ProcessAccess-[ProcessAccessedFrom:TargetProcessGuid]->ProcessCreate
+
+// Stage 4 - Steal
+// ProcessCreate-[RawRead:ProcessGuid,Hostname]->RawAccessRead
+
+// Stage 4 - Tampering (Doesn't mean every FileCreateTime = tampering!)
+// ProcessCreate-[ChangedFileCreateTime:ProcessGuid,Hostname]->FileCreateTime
