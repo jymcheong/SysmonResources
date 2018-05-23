b206e81cc0c7af6ae96e39f7335a88a68fb362ac
jymcheong <jym@jym.sg>
2018-05-23 14:26:39 +0800

    add CreateRemoteThread edges

--- 5. Threat Analytics/orientDB/createEdges.js
+++ 5. Threat Analytics/orientDB/createEdges.js
@@ -2,11 +2,15 @@ var OrientDB = require('orientjs');
 var server = OrientDB({host: 'localhost', port: 2424});
 var db = server.use({name: 'DataFusion', username: 'root', password: 'Password1234', useToken : true});
 
-//create ProcessCreate Parent-Child relationships
+/* Legend: FromClass-[EdgeClassName:PropertiesToLinkWith]->ToClass
+ * Summary: Create ParentOf Edge for ProcessCreate Parent-Child vertices.
+ * Description: Uses OrientDB Live Query to create edges when vertices are inserted.
+ *              ProcessCreate-[ParentOf:ProcessGuid,Hostname]->ProcessCreate 
+ * @param {inserted vertex} data
+*/
 db.liveQuery("live select from ProcessCreate")
-  .on('live-insert', function(data){
-     var child = data.content;
-     // your code here...
+  .on('live-insert', function(inserted){
+     var child = inserted.content;
      console.log('inserted ' + JSON.stringify(child));
      db.query('SELECT @rid FROM ProcessCreate WHERE ProcessGuid = "' 
               + child.ParentProcessGuid + '" AND Hostname = "' + child.Hostname + '"'
@@ -16,18 +20,48 @@ db.liveQuery("live select from ProcessCreate")
                     //create edge between parent to current vertex
                     db.query('CREATE EDGE ParentOf FROM ' + parent[0].rid + 
                     ' TO (SELECT FROM ProcessCreate WHERE ProcessGuid ="' + child.ProcessGuid + 
-                    '" AND Hostname = "' + child.Hostname + 
-                    '")');
+                    '" AND Hostname = "' + child.Hostname + '")');
                   }
                   else
                     console.log('no parent found...')
             });
   })
 // The implementation pattern is very similar to the code above...
-// Legend: FromClass-[EdgeClassName:PropertiesToLinkWith]->ToClass
 // Stage 2 - Run payload ====
-// ProcessCreate-[CreatedRemoteThread:SourceProcessGuid]->CreateRemoteThread
-// CreateRemoteThread-[RemoteThreadFor:TargetProcessId]->ProcessCreate
+// note the use of RecordNumber to ensure we are inserting to the correct vertex.
+db.liveQuery("live select from CreateRemoteThread")
+  .on('live-insert', function(data){
+     var CreateRemoteThread = data.content;
+     console.log('inserted ' + JSON.stringify(CreateRemoteThread));
+     // ProcessCreate-[CreatedRemoteThread:SourceProcessGuid]->CreateRemoteThread
+     db.query('SELECT @rid FROM ProcessCreate WHERE ProcessGuid = "' 
+              + CreateRemoteThread.SourceProcessGuid + '" AND Hostname = "' + CreateRemoteThread.Hostname + '"'
+            ).then(function(ProcessCreate){
+                  if(ProcessCreate.length > 0) { //when ProcessCreate event exist
+                    cmd = 'CREATE EDGE CreatedThread FROM ' + ProcessCreate[0].rid + 
+                          ' TO (SELECT FROM CreateRemoteThread WHERE RecordNumber ="' + CreateRemoteThread.RecordNumber + 
+                          '" AND Hostname = "' + CreateRemoteThread.Hostname + '")';
+                    console.log('command: ' + cmd);
+                    db.query(cmd);
+                  }
+                  else
+                    console.log('ProcessCreate vertex not found...')
+            });
+
+      // CreateRemoteThread-[RemoteThreadFor:TargetProcessId]->ProcessCreate
+      db.query('SELECT @rid FROM ProcessCreate WHERE ProcessId = "' 
+            + CreateRemoteThread.TargetProcessId + '" AND Hostname = "' + CreateRemoteThread.Hostname + '"'
+          ).then(function(ProcessCreate){
+                if(ProcessCreate.length > 0) { //when ProcessCreate event exist
+                  cmd = 'CREATE EDGE RemoteThreadFor FROM (SELECT FROM CreateRemoteThread WHERE RecordNumber = ' 
+                  + CreateRemoteThread.RecordNumber + ' AND Hostname = "' + CreateRemoteThread.Hostname + '") TO ' + ProcessCreate[0].rid;
+                  console.log('command: ' + cmd);
+                  db.query(cmd);
+                }
+                else
+                  console.log('ProcessCreate vertex not found...')
+          });
+  })
 
 // Stage 2 - Install Payload / Persistence ====
 // ProcessCreate-[WroteFile:ProcessGuid,Hostname]->FileCreate
