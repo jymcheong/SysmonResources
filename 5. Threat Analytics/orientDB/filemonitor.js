48d15fa441117b90b02cd98985b60e6900230f16
jymcheong <jym@jym.sg>
2018-06-18 17:05:30 +0800

    ported python logic into processLine

--- 5. Threat Analytics/orientDB/filemonitor.js
+++ 5. Threat Analytics/orientDB/filemonitor.js
@@ -1,7 +1,6 @@
-//==== Configure here ========
-const directory_to_monitor = 'C:/Windows/Datafusion/logs'
+const directory_to_monitor = "C:/Windows/Datafusion/logs";
 var OrientDB = require('orientjs');
-var server = OrientDB({host: 'localhost', port: 2424});
+var server = OrientDB({host: '172.30.1.34', port: 2424});
 var db = server.use({name: 'DataFusion', username: 'root', password: 'Password1234', useToken : true});
 //==================================
 const eventIdLookup = {1:'ProcessCreate', 2:'FileCreateTime', 3:'NetworkConnect', 
@@ -18,16 +17,11 @@ function processFile(filepath) {
     , es = require('event-stream'); //install first: npm i event-stream
     var s = fs.createReadStream(filepath)
         .pipe(es.split())
-        .pipe(es.mapSync(function(line){
-            
+        .pipe(es.mapSync(function(line) {            
             // pause the readstream
             s.pause();
-
             // process line here and call s.resume() when rdy
-            console.log(line)
-
-            //port the python codes here
-            
+            processLine(line)
             // resume the readstream, possibly from a callback
             s.resume();
         })
@@ -36,8 +30,48 @@ function processFile(filepath) {
         })
         .on('end', function(){
             console.log('Read entire file.')
+            //either zip & delete the file.. after a while it's huge.
         })
     );    
+}
+
+//port the python codes here
+function processLine(eventline) {
+    e = JSON.parse(eventline)
+    classname = 'WinEvent'
+    
+    // Sysmon events
+    if(eventline.indexOf('"SourceName":"Microsoft-Windows-Sysmon"') > -1){
+        if(e['Keywords'] != undefined) {
+            e['Keywords'] = '' + e['Keywords']
+        }
+        e['SysmonProcessId'] = e['ProcessID']
+        delete e['ProcessID']
+        var re = /ProcessId: (\d+)/g
+        var match = re.exec(eventline)
+        if(match != null)
+            e['ProcessId'] = parseInt(match[1])
+
+        classname = eventIdLookup[e['EventID']]
+    }
+    
+    // DataFusion events
+    if(eventline.indexOf('"SourceName":"DataFuseUserActions"') > -1){
+        delete e['ProcessID']
+        uat = JSON.parse(e['Message'])
+        for(var k in uat){
+            e[k] = uat[k]
+        }
+        classname = 'UserActionTracking'
+    }
+
+    stmt = 'insert into ' + classname + ' content ' + JSON.stringify(e)
+    console.log(stmt)
+    db.query(stmt).then(function (response){ 
+        //console.log(response)
+        rowCount++
+        console.log('inserted row count: ' + rowCount)
+    });
 }
 
 function startFileMonitor() {
@@ -70,4 +104,6 @@ function startFileMonitor() {
         })
 }
 
-startFileMonitor()
+var rowCount = 0
+//startFileMonitor()
+processFile('/tmp/events.txt')
