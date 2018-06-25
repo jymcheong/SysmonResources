c48a40b9c4ff7bf647e2f23fef55b0901842f2fb
jymcheong <jym@jym.sg>
2018-06-25 17:14:54 +0800

    refactor spacings

--- 5. Threat Analytics/orientDB/testBulkCreate.js
+++ 5. Threat Analytics/orientDB/testBulkCreate.js
@@ -7,38 +7,34 @@ var pchostname = "DESKTOP-O153T4R"
 
 
 function bulkCreateLoadedImage(pcprocessguid,pchostname) {
-      db.query("select count(@rid) from ImageLoad WHERE ProcessGuid = :guid AND Hostname = :hostname AND in().size() = 0",
-         {params:{
-            guid: pcprocessguid,
-            hostname: pchostname},limit: 1}
-        ).then(function(data){
-            newlimit = data[0].count
-            console.log('Create Edge limit: ' + newlimit);
-            if(newlimit > 0) {
-                  db.query("select @rid from ImageLoad WHERE ProcessGuid = :guid AND Hostname = :hostname AND in().size() = 0",
-                        {params:{
-                              guid: pcprocessguid,
-                              hostname: pchostname
-                        },limit: newlimit}
-                        ).then(function(data){
-                              var size = data.length
-                              var TORIDs = '['
-                              data.forEach(element => {
-                                    TORIDs += ('#' + element.rid.cluster + ':' + element.rid.position)
-                                    if(size > 1) {
-                                          TORIDs += ","
-                                    } else{
-                                          TORIDs += "]"
-                                    }
-                                    size -= 1
-                                    }
-                                    );
-                              //console.log(TORIDs)
-                              db.query("create Edge LoadedImage from (SELECT FROM ProcessCreate WHERE \
-                                       ProcessGuid = :guid AND Hostname = :hostname) to " + TORIDs,
-                                       {params:{guid:pcprocessguid,hostname:pchostname}})
-                        })
-            }
+      db.query("select count(@rid) from ImageLoad WHERE ProcessGuid = :guid \
+               AND Hostname = :hostname AND in().size() = 0",
+         {params:{ guid: pcprocessguid,hostname: pchostname},limit: 1}).then(function(data){
+                  newlimit = data[0].count
+                  console.log('Create Edge limit: ' + newlimit);
+                  if(newlimit > 0) {
+                        db.query("select @rid from ImageLoad WHERE ProcessGuid = :guid AND \
+                                  Hostname = :hostname AND in().size() = 0",{ params:{
+                                    guid: pcprocessguid,
+                                    hostname: pchostname
+                              },limit: newlimit}).then(function(data){
+                                    var size = data.length
+                                    var TORIDs = '['
+                                    data.forEach(elem => {
+                                          TORIDs += ('#' + elem.rid.cluster + ':' + elem.rid.position)
+                                          if(size > 1) {
+                                                TORIDs += ","
+                                          } else{
+                                                TORIDs += "]"
+                                          }
+                                          size -= 1
+                                          });
+                                    //console.log(TORIDs)
+                                    db.query("create Edge LoadedImage from (SELECT FROM ProcessCreate WHERE \
+                                          ProcessGuid = :guid AND Hostname = :hostname) to " + TORIDs,
+                                          {params:{guid:pcprocessguid,hostname:pchostname}})
+                              })
+                  }
          })
 } 
 
