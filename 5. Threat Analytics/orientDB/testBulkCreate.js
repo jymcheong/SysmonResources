2ebdcf70bae13fd88bdc7834ca2b616792fc9122
jymcheong <jym@jym.sg>
2018-06-26 17:23:24 +0800

    changed schema, initial test for bulk create ProcessAccess edges function

--- 5. Threat Analytics/orientDB/testBulkCreate.js
+++ 5. Threat Analytics/orientDB/testBulkCreate.js
@@ -38,5 +38,39 @@ function bulkCreateLoadedImage(pcprocessguid,pchostname) {
          })
 } 
 
-setTimeout(() => {
-      bulkCreateLoadedImage(pcprocessguid,pchostname) }, 3000);
+/**
+ * 
+ * @param {event object} ProcessAccess 
+ */
+function bulkCreateProcessAccessed(ProcessAccess){
+      console.log(ProcessAccess.RecordNumber);
+      var PArid = '#' + ProcessAccess["@rid"].cluster + ':' + ProcessAccess["@rid"].position 
+      console.log(PArid);
+      var recno = ProcessAccessLastRecordNo < ProcessAccess.RecordNumber? ProcessAccessLastRecordNo : ProcessAccess.RecordNumber   
+      db.query("SELECT FROM ProcessAccess WHERE RecordNumber >= :recordno AND Hostname = :hostname AND in().size() = 0 AND out().size() = 0 Order By RecordNumber", 
+               {params:{recordno: recno, hostname: ProcessAccess.Hostname}, limit:500}
+              ).then(function(results){
+                  console.log(results.length)
+                  results.forEach(item => {
+                        console.log(item.SourceProcessGUID)
+                        // create edge ProcessAccess -[ProcessAccessedTo]-> ProcessCreate
+                        db.query("create Edge ProcessAccessedTo from :rid to (SELECT FROM ProcessCreate WHERE \
+                              ProcessGuid = :guid AND Hostname = :hostname Order By EventTime LIMIT 1)",
+                              {params:{rid: item["@rid"], guid:item.TargetProcessGUID, hostname:item.Hostname}})
+                        // create edge ProcessCreate -[ProcessAccessedFrom]-> ProcessAccess
+                        db.query("create Edge ProcessAccessedFrom from (SELECT FROM ProcessCreate WHERE \
+                              ProcessGuid = :guid AND Hostname = :hostname Order By EventTime LIMIT 1) to :rid",
+                              {params:{guid:item.SourceProcessGUID, hostname:item.Hostname, rid: item["@rid"]}})
+                        ProcessAccessLastRecordNo = item.RecordNumber
+                        console.log(ProcessAccessLastRecordNo)
+                  })
+               })
+}
+
+//setTimeout(() => { bulkCreateLoadedImage(pcprocessguid,pchostname) }, 3000);
+
+var ProcessAccessLastRecordNo = 17177333
+
+db.query('SELECT FROM ProcessAccess WHERE RecordNumber = 17177333').then(function(data){
+      bulkCreateProcessAccessed(data[0])
+})
