52cce66bff069a174cd517f537e706448dc32b51
jymcheong <jym@jym.sg>
2018-06-28 13:44:03 +0800

    added server function example script

---
+++ 5. Threat Analytics/orientDB/sampleServerFunction.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * EXPORT command does not save server functions out
+ * Here's a basic javascript example. 
+ * Need to turn 'on' javascript language since it is disabled by default (less secure if clients are untrusted)
+ * Paste the whole thing into Function Management script editor & so on to 'install' 
+ */
+
+var db = orient.getDatabase();
+
+// positional parameters for queries eg. when function has inputs, always use parameterized query!
+var r = db.query('select from ProcessAccess where ToBeProcessed = true Order By EventTime LIMIT ?', 100);
+
+// this prints to the console where DB was started
+print(r.length)
+
+// for each record processing example loop
+for(var i = 0; i < r.length; i++) {
+    print(r[i].getProperty('@rid')) //accessing a property/field
+}
+
+// besides returning results to caller, one can see the contents within below
+return r 
