0f942f466d4a79721c6af65b94678271f2e13b11
jymcheong <jym@jym.sg>
2018-07-02 10:53:34 +0800

    partial progress

--- 5. Threat Analytics/orientDB/ServerSideAddEvent.js
+++ 5. Threat Analytics/orientDB/ServerSideAddEvent.js
@@ -5,66 +5,91 @@
  */
 var db = orient.getDatabase();
 
-//need to do this since it won't return the record
-var e = JSON.parse(jsondata); 
-print(e["ProcessGuid"]) // need this to find ProcessCreate eg. ProcessGuid
+// edge class look up table
+var edgeLookup = {'ProcessTerminate':'Terminated', 'PipeCreated':'CreatedPipe',
+                  'PipeConnected':'ConnectedPipe', 'RawAccessRead':'RawRead',
+                  'FileCreateTime':'ChangedFileCreateTime', 'FileCreate':'CreatedFile',
+                  'FileCreateStreamHash':'CreatedFileStream', 'RegistryEvent':'AccessedRegistry',
+                  'NetworkConnect':'ConnectedTo', 'ImageLoad':'LoadedImage'}
 
-// Insert cannot use parameters. RETURN @this will crash the whole system
+var e = JSON.parse(jsondata); 
 var r = db.command('insert into '+ classname + ' content ' + jsondata);
+var stmt = ''
 
 switch(classname) {
-// ProcessCreate-[ParentOf:ProcessGuid,Hostname]->ProcessCreate
+
+    // ProcessCreate-[ParentOf:ParentProcessGuid,Hostname]->ProcessCreate
   case "ProcessCreate":
-    
-// ProcessCreate-[CreatedRemoteThread:SourceProcessGuid]->CreateRemoteThread
-// CreateRemoteThread-[RemoteThreadFor:TargetProcessId]->ProcessCreate
-  case "CreateRemoteThread":
-    
-// ProcessCreate-[WroteFile:ProcessGuid,Hostname]->FileCreate
-// FileCreate-[UsedAsDriver:TargetFilename=ImageLoaded]->DriverLoad
-  case "FileCreate":
+    	stmt = 'CREATE EDGE ParentOf FROM (SELECT FROM ProcessCreate WHERE ProcessGuid = ? AND Hostname = ?) TO ?'
+        try{
+    		db.command(stmt,e['ParentProcessGuid'], e['Hostname'],r[0].getProperty('@rid'))
+        }
+    	catch(err){
+        	print('parent process not found')
+        }
+    	break;
 
-// ProcessCreate-[LoadedImage:ProcessGuid,Hostname]->ImageLoad
-// FileCreate-[UsedAsImage:TargetFilename=ImageLoaded]->ImageLoad
-  case "ImageLoad": // implemented as bulk process function later
-    
-// ProcessCreate-[AccessedRegistry:ProcessGuid,Hostname]->RegistryEvent
-// FileCreateStreamHash-[FoundWithin:TargetFilename in Details]->RegistryEvent
-// this assumes ADS created first before the registry event; the reverse can happen too
-  case "RegistryEvent":
-
-// ProcessCreate-[CreatedFileStream:ProcessGuid,Hostname]->FileCreateStreamHash   
-// FileCreateStreamHash-[FoundWithin:TargetFilename in Details]->RegistryEvent
-  case "FileCreateStreamHash":    
-    
-// ProcessCreate-[Terminated:ProcessGuid,Hostname]->ProcessTerminate
-  case "ProcessTerminate":      
-
-// ProcessCreate-[ConnectedTo:ProcessGuid,Hostname]->NetworkConnect
-// If destination IP/host is also in the population, will there be a NetworkConnect of the other host?
-// anyway to look at it, is there a NetworkConnect where Source IP is from another machine?
-// NetworkConnect-[ConnectedTo:(L.Hostname = R.SourceHostname) & L.Hostname != R.Hostname]->NetworkConnect  <- need to consider time window
-  case "NetworkConnect": 
-
-// ProcessCreate-[CreatedPipe:ProcessGuid,Hostname]->PipeCreated
-  case "PipeCreated":
-
-// ProcessCreate-[ConnectedPipe:ProcessGuid,Hostname]->PipeConnected
+    // ProcessCreate-[Terminated:ProcessGuid,Hostname]->ProcessTerminate
+  case "ProcessTerminate":      	
+    // ProcessCreate-[CreatedPipe:ProcessGuid,Hostname]->PipeCreated
+  case "PipeCreated":	
+    // ProcessCreate-[ConnectedPipe:ProcessGuid,Hostname]->PipeConnected
   case "PipeConnected":
-
-// ProcessCreate-[ProcessAccessed:L.ProcessGuid = R.SourceProcessGUID]->ProcessAccess
-// ProcessAccess-[ProcessAccessedFrom:L.TargetProcessGUID = R.ProcessGuid]->ProcessCreate
-// another bulk processing function for this    
-  case "ProcessAccess":
-    
-// ProcessCreate-[RawRead:ProcessGuid,Hostname]->RawAccessRead
+    // ProcessCreate-[RawRead:ProcessGuid,Hostname]->RawAccessRead
   case "RawAccessRead":
+	// ProcessCreate-[ChangedFileCreateTime:ProcessGuid,Hostname]->FileCreateTime
+  case "FileCreateTime":		
+    // ProcessCreate-[CreatedFile:ProcessGuid,Hostname]->FileCreate
+  case "FileCreate":
+	// ProcessCreate-[CreatedFileStream:ProcessGuid,Hostname]->FileCreateStreamHash   
+  case "FileCreateStreamHash":    
+    // ProcessCreate-[AccessedRegistry:ProcessGuid,Hostname]->RegistryEvent
+  case "RegistryEvent":
+    // ProcessCreate-[ConnectedTo:ProcessGuid,Hostname]->NetworkConnect
+  case "NetworkConnect": 
+	// ProcessCreate-[LoadedImage:ProcessGuid,Hostname]->ImageLoad    
+   case "ImageLoad":
+    	stmt = 'CREATE EDGE ? FROM (SELECT @rid FROM ProcessCreate WHERE ProcessGuid = ? AND Hostname = ?) TO ?'
+    	try{
+          db.command(stmt,edgeLookup[classname],e['ProcessGuid'],e['Hostname'],r[0].getProperty('@rid'))
+        }
+    	catch(err){
+        }
+		break;
+    
+    // FileCreate-[UsedAsDriver:TargetFilename=ImageLoaded]->DriverLoad
+  case "DriverLoad":
+    	break;
 
-// ProcessCreate-[ChangedFileCreateTime:ProcessGuid,Hostname]->FileCreateTime
-  case "FileCreateTime":
+    // ProcessCreate-[CreatedRemoteThread:SourceProcessGuid]->CreateRemoteThread
+    // CreateRemoteThread-[RemoteThreadFor:TargetProcessId]->ProcessCreate
+  case "CreateRemoteThread":
+    	break;
     
+    // ProcessCreate-[LoadedImage:ProcessGuid,Hostname]->ImageLoad
+    // FileCreate-[UsedAsImage:TargetFilename=ImageLoaded]->ImageLoad
+//  case "ImageLoad": // for bulk process function    
     
-    print(classname)
+	// ProcessCreate-[ProcessAccessed:L.ProcessGuid = R.SourceProcessGUID]->ProcessAccess
+	// ProcessAccess-[ProcessAccessedFrom:L.TargetProcessGUID = R.ProcessGuid]->ProcessCreate
+//  case "ProcessAccess": // for bulk process function
+
+//	case 'UserActionTracking':    
 }
 
-return r 
+print(classname)
+    
+//process classes with 2nd possible edge
+if(classname == "NetworkConnect"){
+// NetworkConnect-[ConnectedTo:(L.Hostname = R.SourceHostname) & L.Hostname != R.Hostname]->NetworkConnect
+}
+    
+if(classname == "RegistryEvent"){
+// FileCreateStreamHash-[FoundWithin:TargetFilename in Details]->RegistryEvent
+}
+    
+if(classname == "FileCreateStreamHash"){
+// FileCreateStreamHash-[FoundWithin:TargetFilename in Details]->RegistryEvent
+}
+
+//return r 
