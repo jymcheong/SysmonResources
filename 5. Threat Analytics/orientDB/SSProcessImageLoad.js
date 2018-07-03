f68faec6423e96bb08c80ecf6b5b43f23132aa01
jc <jym@jym.sg>
2018-07-03 21:52:42 +0800

    needs Order By

--- 5. Threat Analytics/orientDB/SSProcessImageLoad.js
+++ 5. Threat Analytics/orientDB/SSProcessImageLoad.js
@@ -11,5 +11,5 @@
 // 3. Update Function Status to "running" state
 // 4. Select ProcessCreate RID, Hostname & ProcessGuid matching first N ImageLoad rows sorted by EventTime >= startTime
 // 5. Foreach rid & ProcessGuid, create edges from ProcessCreate RID to (ImageLoad with matching Hostname, ProcessGuid and EventTime >= startTime sorted by EventTime LIMIT N)
-// 6. Update ToBeProcessed = false where EventTime >= startTime LIMIT N
+// 6. Update ToBeProcessed = false where EventTime >= startTime sorted by EventTime LIMIT N
 // 7. Update Function Status to "stopped" state
