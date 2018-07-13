3d3fe4d573858970f6198368c57c4ffea368b65f
jymcheong <jym@jym.sg>
2018-07-13 11:50:51 +0800

    for nodejs

---
+++ 5. Threat Analytics/orientDB/installFileMonNodejs.ps1
@@ -0,0 +1,24 @@
+# Use a Admin CMD console & type:  powershell -ExecutionPolicy Bypass -File installsysmonviz.ps1
+$nxlogpath = "c:\sysmonviz\nxlog"
+$p = [Environment]::GetFolderPath("Desktop") + "\sysmonviz"
+New-Item -Force -ItemType directory -Path $p
+cd $p
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/jymcheong/SysmonResources/master/5.%20Threat%20Analytics/orientDB/filemonitor.js", [Environment]::GetFolderPath("Desktop") + "\sysmonviz\filemonitor.js")
+
+# install chocolatey 
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install nodejs --version 8.2.0 -y
+
+# install nodejs
+choco install microsoft-visual-cpp-build-tools -y 
+npm install --global --production windows-build-tools
+npm install --global node-gyp
+
+# something wrong with global in windows... install as local modules
+npm install nsfw
+npm install event-stream
+npm install orientjs
+
+$filemonscript = Get-Content filemonitor.js
+$filemonscript = $filemonscript -replace 'C:/Windows/Datafusion/logs', 'C:/sysmonviz/logs'
+$filemonscript | Set-Content "$p\filemonitor.js"
