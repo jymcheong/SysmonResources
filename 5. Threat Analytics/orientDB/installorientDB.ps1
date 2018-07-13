fd9871cb920ab0805bdc8025165a5261ad2d9e05
jymcheong <jym@jym.sg>
2018-07-13 11:11:42 +0800

    installation scripts

---
+++ 5. Threat Analytics/orientDB/installorientDB.ps1
@@ -0,0 +1,26 @@
+$root_pass = 'Password1234'
+cd ~
+cd Desktop
+$p = [Environment]::GetFolderPath("Desktop") + "\orientDBsetup"
+New-Item -Force -ItemType directory -Path $p
+cd $p
+
+# download ODB 3.0.3
+Import-Module BitsTransfer
+Start-BitsTransfer -Source https://s3.us-east-2.amazonaws.com/orientdb3/releases/3.0.3/orientdb-3.0.3.zip -Destination "$p\orientdb.zip"
+
+# unzip orientdb.zip
+$shell = New-Object -ComObject Shell.Application
+$zip = $shell.NameSpace(“$p\orientdb.zip"); foreach($item in $zip.items()) { $shell.Namespace($p).copyhere($item) }
+
+# start orientDB with root pass configured above
+[Environment]::SetEnvironmentVariable("ORIENTDB_ROOT_PASSWORD",$root_pass)
+[Environment]::SetEnvironmentVariable("ORIENTDB_HOME", "$p\orientdb-3.0.3")
+
+# check for java in path, quit if don't exist, no point continuing
+Start-Process -FilePath "$env:comspec"  -ArgumentList "/c orientdb-3.0.3\bin\server.bat"
+
+Write-host "\n"
+Read-Host -Prompt  "Please check the orientDB server console for 'Server is Active' before pressing any key to continue"
+Start-BitsTransfer -Source https://github.com/jymcheong/SysmonResources/raw/master/5.%20Threat%20Analytics/orientDB/schema_and_functions.gz -Destination "$p\schema_and_functions.gz"
+orientdb-3.0.3\bin\server.bat "create database remote:localhost\DataFusion root $root_pass; import database schema_and_functions.gz"
