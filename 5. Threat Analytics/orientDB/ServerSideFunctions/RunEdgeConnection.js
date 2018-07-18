1268c864854b8701643abc46097bfdf7514ce51a
jymcheong <jym@jym.sg>
2018-07-18 13:32:08 +0800

    scheduled task to call server side function

---
+++ 5. Threat Analytics/orientDB/ServerSideFunctions/RunEdgeConnection.js
@@ -0,0 +1,12 @@
+/**
+* 1. Use ODB WebStudio Function Management
+* 2. Create a function RunEdgeConnection
+* 3. Paste the codes below into the FM's editor & save  
+* 4. Use ODB Console or Browse tab to issue this statement to run it every 5 seconds:
+
+INSERT INTO oschedule SET name = 'ConnectEdges', function = (SELECT FROM ofunction WHERE name = 'RunEdgeConnection'), rule = "0/5 * * * * ?"
+*/
+
+var db = orient.getDatabase();
+db.query("select ConnectImageLoad()")
+db.query("select ConnectProcessAccess()")
