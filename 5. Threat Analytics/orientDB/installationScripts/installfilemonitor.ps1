67ccb8f6e548c8b3686cc36e99ea7d9d8f4033c9
jymcheong <jym@jym.sg>
2018-07-31 15:54:56 +0800

    fixed

--- 5. Threat Analytics/orientDB/installationScripts/installfilemonitor.ps1
+++ 5. Threat Analytics/orientDB/installationScripts/installfilemonitor.ps1
@@ -1,4 +1,4 @@
-$ODBhost =$args[0]
+$ODBhost = $args[0]
 # Use admin CMD console & type:  powershell -ExecutionPolicy Bypass -File installsysmonviz.ps1
 
 # Everything will go into this folder on your desktop
