b58be3f6fec889b5191413d12455f22774212603
jymcheong <jym@jym.sg>
2018-08-08 09:39:01 +0800

    updated

--- 5. Threat Analytics/orientDB/installationScripts/installorientDB.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Other scripts use this default password. Change the rest if you change the default
-export ORIENTDB_ROOT_PASSWORD=Password1234
-export ORIENTDB_HOME=`pwd`
-wget https://s3.us-east-2.amazonaws.com/orientdb3/releases/3.0.4/orientdb-3.0.4.tar.gz
-wget https://github.com/jymcheong/SysmonResources/raw/master/5.%20Threat%20Analytics/orientDB/schema_and_functions.gz
-tar zxvf orientdb-3.0.4.tar.gz 
-orientdb-3.0.4/bin/server.sh &
-orientdb-3.0.4/bin/console.sh "create database remote:localhost\DataFusion root $ORIENTDB_ROOT_PASSWORD; import database schema_and_functions.gz;"
